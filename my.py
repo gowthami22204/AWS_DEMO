6c8dda0fe27eb419c399d05bb9b3e2fb5d7606bd
gowthami <gowthami@gmail.com>
2024-01-25 17:11:56 +0530

    changes

---
+++ my.py
@@ -0,0 +1,81 @@
+####Pattern 1: Pyramid Pattern
+##
+##def pattern(n):
+##    for i in range(0,n):
+##        for j in range(0,i+1):
+##            print(' * ',end="")
+##        print("\r")
+##
+##obj=pattern(5)
+##print(obj)
+##==========================================            
+
+#reverse pyramid using while loop:
+##
+##n=5;
+##i=0
+##while (i<=n):
+##    print(" "*(n-i)+"*"*i)#'5-1 (4 spaces *"*"*1==>    *
+##    i+=1
+
+##height=5
+##for i in range(0,height):
+##    print(""*(height-i)+"*" *i)
+##
+##
+
+##height=5
+##for i in range(0,height):
+##    print("" *(height+i)+"*" *i)
+        
+#========================================= 
+####Pattern 3: Diamond Pattern
+##def diamond_pattern(n):
+##   
+##    for i in range(0, n):
+##        for j in range(0, n - i - 1):
+##            print(" ", end=" ")
+##        for j in range(0, i + 1):
+##            print("*", end=" ")
+##        print("\r")
+##
+##    for i in range(n - 1, 0, -1):
+##        for j in range(0, n - i):
+##            print(" ", end=" ")
+##        for j in range(0, i):
+##            print("*", end=" ")
+##        print("\r")
+##
+##diamond_pattern(5)
+
+
+
+
+
+
+
+
+
+
+
+"""
+i added something
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
